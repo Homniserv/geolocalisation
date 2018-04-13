5553afbf811002f5d345da73fafd3091af3dfcc3
Peter MOUEZA <mouezapeter@gmail.com>
2018-04-13 14:37:58 +0200

    sov

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
-(function(){
-var x = document.getElementById("demo");
+(function () {
+    var x = document.getElementById("demo");
     console.log("in JS");
-function getLocation() {
-    if (navigator.geolocation) {
-        console.log("geo KO");
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    function getLocation() {
+        if (navigator.geolocation) {
+            console.log("geo KO");
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
     }
-}
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-}
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+    }
 
 
 })();
